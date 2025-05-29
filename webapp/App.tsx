84b25f01c86e222f103a2025797c07ff9176d1c2
widanet <willianlps99@outlook.com>
2025-05-29 14:16:05 -0300

    update

--- webapp/App.tsx
+++ webapp/App.tsx
@@ -2,19 +2,16 @@ import 'react-native-get-random-values';
 import React from "react";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-// Removendo a importação do global.css para evitar o erro de renderização de texto
-// import "./src/styles/global.css";
 
 import Routes from "./src/routes/index";
 import { AuthProvider } from "./src/contexts/AuthContext";
-import Chat from './src/pages/chat';
 
 const App = () => {
   return (
     <NavigationContainer>
       <AuthProvider>
         <StatusBar backgroundColor="#1b86ea" barStyle="light-content" />
-        <Chat />
+        <Routes />
       </AuthProvider>
     </NavigationContainer>
   );
