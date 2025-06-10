baf6c537dcbb7d6644dbdce2926ccb5d535accdf
Alberth Luiz <alberth-luiz-data>
2025-06-10 20:27:03 -0300

    Register

--- desconet-web/src/routes/index.jsx
+++ desconet-web/src/routes/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "../pages/Welcome";
 import Login from "../pages/Login";
-// import Register from "../pages/Register";
+import Register from "../pages/Register";
 
 
 const routes = [
   { path: "/", element: <Welcome /> },
   { path: "/login", element: <Login /> },
-  // { path: "/register", element: <Register /> },
+  { path: "/register", element: <Register /> },
+  
 ];
 
 export default routes;
